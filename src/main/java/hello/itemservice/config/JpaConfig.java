4800ddaca7e9c96711ed03116effcbdd48a8eb73
서민재 <seominjae@seominjaeui-MacBookPro.local>
2024-02-13 22:59:59 +0900

    ⭐ feat: 스프링 데이터 JPA 적용 2 수강 중

--- src/main/java/hello/itemservice/config/JpaConfig.java
+++ src/main/java/hello/itemservice/config/JpaConfig.java
@@ -1,8 +1,7 @@
 package hello.itemservice.config;
 
 import hello.itemservice.repository.ItemRepository;
-import hello.itemservice.repository.jpa.JpaItemRepository;
-import hello.itemservice.repository.mybatis.MyBatisItemRepository;
+import hello.itemservice.repository.jpa.JpaItemRepositoryV1;
 import hello.itemservice.service.ItemService;
 import hello.itemservice.service.ItemServiceV1;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +23,6 @@ public class JpaConfig {
 
     @Bean
     public ItemRepository itemRepository() {
-        return new JpaItemRepository(em);
+        return new JpaItemRepositoryV1(em);
     }
 }
